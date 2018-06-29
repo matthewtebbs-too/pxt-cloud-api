69cf52a4cc956d86e7c6cf6b2e211cca719b3620
Matthew Tebbs <matthew@muddytummy.com>
2018-06-28 22:26:02 -0700

    Fix typo (for real).

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -1,60 +1,42 @@
-
-/*
-    MIT License
-
-    Copyright (c) 2018 MuddyTummy Software LLC
-*/
-
 import { DataSource } from './datarepo';
-
-export enum Events {
-    ChatNewMessage = 'new message',
-    UserAddSelf = 'add self',
-    UserLeft = 'user left',
-    UserJoined = 'user joined',
-    UserRemoveSelf = 'remove self',
-    UserSelfInfo = 'self info',
+export declare enum Events {
+    ChatNewMessage = "new message",
+    UserAddSelf = "add self",
+    UserLeft = "user left",
+    UserJoined = "user joined",
+    UserRemoveSelf = "remove self",
+    UserSelfInfo = "self info"
 }
-
 export interface CommonAPI {
     isConnected: boolean;
     on(event: string | symbol, listener: (...args: any[]) => void): this;
     off(event: string | symbol, listener: (...args: any[]) => void): this;
 }
-
-export type UserId = string;
-
+export declare type UserId = string;
 export interface UserData {
     readonly name: string;
-
-    readonly id?: UserId;   /* reply only */
+    readonly id?: UserId;
 }
-
 export interface UsersAPI extends CommonAPI {
     selfInfo(): PromiseLike<UserData>;
     addSelf(user: UserData): PromiseLike<boolean>;
     removeSelf(): PromiseLike<boolean>;
 }
-
 export interface MessageData {
     readonly text: string;
-
-    readonly name?: string; /* reply only */
+    readonly name?: string;
 }
-
 export interface ChatAPI extends CommonAPI {
     newMessage(msg: string | MessageData): PromiseLike<void>;
 }
-
 export interface WorldAPI extends CommonAPI {
     addDataSource(name: string, source_: DataSource): boolean;
     removeDataSource(name: string): boolean;
     syncData(name: string): PromiseLike<string[]>;
-    syncDiff(name: string, diff: any | any[] /* deep-diff's IDiff */): PromiseLike<string[]>;
+    syncDiff(name: string, diff: any | any[]): PromiseLike<string[]>;
 }
-
 export interface PublicAPI {
-    readonly chat: ChatAPI;    /* namespace is '/pxt-cloud/chat' */
-    readonly users: UsersAPI;  /* namespace is '/pxt-cloud/users' */
-    readonly world: WorldAPI;  /* namespace is '/pxt-cloud/world' */
+    readonly chat: ChatAPI;
+    readonly users: UsersAPI;
+    readonly world: WorldAPI;
 }

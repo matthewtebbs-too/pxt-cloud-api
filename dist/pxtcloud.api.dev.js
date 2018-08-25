45e9bc88af8c1d7970539966e9c2c4a7ce3d4eba
Matthew Tebbs <matthew@muddytummy.com>
2018-08-25 12:57:46 -0700

    Update packages.

--- dist/pxtcloud.api.dev.js
+++ dist/pxtcloud.api.dev.js
@@ -2018,8 +2018,8 @@ function numberIsNaN (obj) {
       define('DeepDiff', function() { // eslint-disable-line no-undef
           return deepDiff;
       });
-  } else if (typeof exports === 'object') {
-      // Node.js
+  } else if (typeof exports === 'object' || typeof navigator === 'object' && navigator.product.match(/ReactNative/i)) {
+      // Node.js or ReactNative
       module.exports = deepDiff;
   } else {
       // Browser globals
@@ -2530,7 +2530,10 @@ function numberIsNaN (obj) {
   // const DeepDiff = require('deep-diff');
   // const { DeepDiff } = require('deep-diff');
 
-  root.DeepDiff = accumulateDiff;
+  if (root) {
+    root.DeepDiff = accumulateDiff;
+  }
+
   return accumulateDiff;
 }));
 
